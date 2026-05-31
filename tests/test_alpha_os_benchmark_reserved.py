aea3c6c687a760d784195e2c7e694f18365618de
tomato414941 <tomato414941@gmail.com>
2026-05-31 10:03:30 +0000

    refactor: remove reserved benchmark stub

--- tests/test_alpha_os_benchmark_reserved.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-def test_benchmark_name_is_reserved_for_trading_comparison_references():
-    from alpha_os.benchmark import Benchmark
-
-    with pytest.raises(NotImplementedError, match="Use EvaluationSpec"):
-        Benchmark()
